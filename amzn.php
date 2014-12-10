5bde6ef1ae9bdcc7699d35a0e5d31d7aac5f6a9c
volcimaster <volcimaster@gmail.com>
2014-12-10 11:50:44 -0500

    Create amzn.php

---
+++ amzn.php
@@ -0,0 +1,3 @@
+<div id="amzn">
+<SCRIPT charset="utf-8" type="text/javascript" src="http://ws.amazon.com/widgets/q?rt=tf_sw&ServiceVersion=20070822&MarketPlace=US&ID=V20070822/US/antipaucity-20/8002/e4b77651-ef80-4fec-967f-fa26e034de67"> </SCRIPT> <NOSCRIPT><A HREF="http://ws.amazon.com/widgets/q?rt=tf_sw&ServiceVersion=20070822&MarketPlace=US&ID=V20070822%2FUS%2Fantipaucity-20%2F8002%2Fe4b77651-ef80-4fec-967f-fa26e034de67&Operation=NoScript">Amazon.com Widgets</A></NOSCRIPT>
+</div>
